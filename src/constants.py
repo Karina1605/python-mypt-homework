474a360007f0dc6f588f3c659a6cdc3c17426efb
DESKTOP-RR8873I\Lenovo <k.akchurina@of-group.ru>
2023-10-03 11:39:00 +0300

    task-1 write predict finction

---
+++ src/constants.py
@@ -0,0 +1,4 @@
+# As we know boundaries od the generated number, we can set an interval
+# number can change in diapazone (0..101)
+HIGH_BOUND = 100
+LOW_BOUND = 1
---
+++ src/predict.py
@@ -0,0 +1,36 @@
+# Import constants for our function
+from constants import *
+
+def game_core(number: int = 1) -> int:
+    """Search based on binary search algorithm.
+
+    Args:
+        number (int, optional): number supposed to predict
+
+    Returns:
+        int: count of attempts
+    """
+    if number < LOW_BOUND or number > HIGH_BOUND:
+        raise ValueError("Generated number was out of set boundaries")
+    
+    # Set interval boundaries
+    high_bond = HIGH_BOUND + 1
+    low_bond = LOW_BOUND - 1
+
+    # Attempts count
+    count = 0
+    
+    # Our prediction - the middle of the interval
+    predict = (high_bond - low_bond)//2
+    
+    while predict != number:
+        count += 1
+        if (predict > number):
+            # Cut all impossible values, which are greater than current
+            high_bond = predict
+        else:
+            # Cut all impossible values, which are less than current
+            low_bond =predict
+        # Next prediction - the middle of the cut interval
+        predict = low_bond + (high_bond -low_bond) //2 
+    return count
